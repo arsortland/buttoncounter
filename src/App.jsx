34f38c33761f26b6b38051031e02dea1ffd3dd3f
André Sortland <arsortland@gmail.com>
2023-06-29 22:50:15 +0200

    testing default react git settings

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <div className="App">
+    <button onClick={() => setCount(count + 1)}>Klikk meg</button>
+    <h1>{count}</h1>
+  </div>;
+}
+
+export default App;
